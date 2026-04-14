c64b822e7f466f3188e8c5f47cd45e5b5cffd7df
Kauan Farias <kauandefarias@yahoo.com.br>
2026-04-14 20:12:35 -0300

    feat(config): adicionar .env e conexão inicial com o banco de dados
    Para que as variáveis de conexão com o Supabase no arquivo .env sejam lidas e usadas, a dependência "dotenv-java" foi adicionada ao projeto.

--- src/br/edu/brazcubas/fitbase/app/FitBase.java
+++ src/br/edu/brazcubas/fitbase/app/FitBase.java
@@ -1,7 +1,20 @@
 package br.edu.brazcubas.fitbase.app;
 
+import java.sql.Connection;
+
+import br.edu.brazcubas.fitbase.db.DatabaseConnection;
+
 public class FitBase {
 	public static void main(String[] args) {
-		System.out.println("Olá, mundo!");
+		// Verifica a conexão com o banco de dados
+		System.out.println("Iniciando conexão com o Supabase...");
+
+		try (Connection conn = DatabaseConnection.getConnection()) {
+			if (conn != null) { // Se não retornar nulo, deu certo
+				System.out.println("\u001B[1;32m[SUCESSO]\u001B[0m Supabase - Conexão estabelecida.");
+			}
+		} catch (Exception e) { // Se retornar nulo, mostra o erro
+			System.out.println(e.getMessage());
+		}
 	}
 }
---
+++ src/br/edu/brazcubas/fitbase/db/DatabaseConnection.java
@@ -0,0 +1,27 @@
+package br.edu.brazcubas.fitbase.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class DatabaseConnection {
+	// Carrega o arquivo .env na memória
+	private static final Dotenv dotenv = Dotenv.load();
+
+	// Obtém os valores do .env para acesso ao banco de dados
+	private static final String URL = dotenv.get("DB_URL");
+	private static final String USER = dotenv.get("DB_USER");
+	private static final String PASS = dotenv.get("DB_PASS");
+
+	// Método para se conectar com o banco de dados
+	public static Connection getConnection() {
+		try { // Tenta estabelecer uma conexão com os valores das variáveis
+			return DriverManager.getConnection(URL, USER, PASS);
+		} catch (SQLException e) { // Se não ocorrer bem, lança um erro
+			throw new RuntimeException("\u001B[1;31m[ERRO]\u001B[0m Supabase - Conexão não estabelecida: \u001B[3m"
+					+ e.getMessage() + "\u001B[0m");
+		}
+	}
+}
