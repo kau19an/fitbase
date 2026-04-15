9d4472a67e5afb0e66577aebc5b9cfde618420a6
Kauan Farias <kauandefarias@yahoo.com.br>
2026-04-14 21:41:13 -0300

    feat(app): preparar esqueleto do menu CRUD + validação de status

--- src/br/edu/brazcubas/fitbase/app/FitBase.java
+++ src/br/edu/brazcubas/fitbase/app/FitBase.java
@@ -2,19 +2,31 @@ package br.edu.brazcubas.fitbase.app;
 
 import java.sql.Connection;
 
-import br.edu.brazcubas.fitbase.db.DatabaseConnection;
+import br.edu.brazcubas.fitbase.db.Supabase;
+
+/**
+ * @author Kauan Farias
+ * @version 1.2
+ */
 
 public class FitBase {
 	public static void main(String[] args) {
-		// Verifica a conexão com o banco de dados
-		System.out.println("Iniciando conexão com o Supabase...");
+		// 1. Verifica a conexão com o banco de dados (Supabase)
+		boolean status = false;
 
-		try (Connection conn = DatabaseConnection.getConnection()) {
+		try (Connection conn = Supabase.getConnection()) {
 			if (conn != null) { // Se não retornar nulo, deu certo
-				System.out.println("\u001B[1;32m[SUCESSO]\u001B[0m Supabase - Conexão estabelecida.");
+				status = true;
 			}
 		} catch (Exception e) { // Se retornar nulo, mostra o erro
+			status = false;
 			System.out.println(e.getMessage());
+		}
+		
+		if (status != true) {
+			System.exit(0);
+		} else {
+			// Menu
 		}
 	}
 }
