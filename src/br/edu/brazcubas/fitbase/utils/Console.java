e4d46c99b183462a6aede4d6bdbd5b70a0d37e5f
Kauan Farias <kauandefarias@yahoo.com.br>
2026-05-01 16:06:07 -0300

    feat(utils): adicionar classe Console para limpeza de tela

---
+++ src/br/edu/brazcubas/fitbase/utils/Console.java
@@ -0,0 +1,15 @@
+package br.edu.brazcubas.fitbase.utils;
+
+/**
+ * @author Kauan Farias
+ * @version 1.0
+ */
+
+public class Console {
+	// Pula 50 linhas ao voltar nos submenus
+	public static void limpar() {
+		for (int i = 0; i < 50; i++) {
+			System.out.println();
+		}
+	}
+}
