e5316eda37ec00b33319b9f49fda1c34c4566a3c
Kauan Farias <kauandefarias@yahoo.com.br>
2026-05-01 21:31:21 -0300

    refactor(entities): adicionar construtor vazio e sobrescrição de 'toString()' no aluno

--- src/br/edu/brazcubas/fitbase/entities/Aluno.java
+++ src/br/edu/brazcubas/fitbase/entities/Aluno.java
@@ -1,10 +1,11 @@
 package br.edu.brazcubas.fitbase.entities;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author Kauan Farias
- * @version 1.0
+ * @version 1.1
  */
 
 public class Aluno {
@@ -19,8 +20,10 @@ public class Aluno {
 	private String telefone;
 	private LocalDate dataMatricula;
 	private Plano plano; // FK: id
+	
+	// Construtores
+	public Aluno() {}
 
-	// Construtor
 	public Aluno(Integer id, String primeiroNome, String meioNome, String ultimoNome, String cpf, LocalDate dataNasc,
 			String email, String telefone, LocalDate dataMatricula, Plano plano) {
 		this.id = id;
@@ -75,7 +78,7 @@ public class Aluno {
 	public Plano getPlano() {
 		return plano;
 	}
-
+	
 	// Setters
 	public void setId(Integer id) {
 		this.id = id;
@@ -115,5 +118,34 @@ public class Aluno {
 
 	public void setPlano(Plano plano) {
 		this.plano = plano;
+	}
+	
+	@Override
+	public String toString() {
+		// Para que a data seja exibida no formato usado no Brasil
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+	    // Se o nome do meio for nulo ou vazio, ignora; senão, o exibe
+	    String meio = (meioNome == null || meioNome.trim().isEmpty()) ? "" : meioNome + " ";
+	    
+	    // Junta cada parte do nome, eliminando os espaços antes e depois (se houver)
+	    String nomeCompleto = (primeiroNome + " " + meio + ultimoNome).trim();
+	    
+	    // Retorna os dados do aluno já formatados para exibir na lista
+	    return String.format("""
+	            [%d] %s
+	            - CPF: %s
+	            - Data de nascimento: %s
+	            - E-mail: %s
+	            - Telefone: %s
+	            - Data de matrícula: %s
+	            ------------------------------------------""",
+	            id, nomeCompleto.toUpperCase(), // Para fins de destaque
+	            cpf,
+	            (dataNasc != null ? dataNasc.format(formato) : "Não informada"), 
+	            email,
+	            telefone,
+	            (dataMatricula != null ? dataMatricula.format(formato) : "Não informada")
+	    );
 	}
 }
